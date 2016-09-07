42cf4f241a82d2aca940ebe7ed960fb934625735
Wayne Aulner <wa6c6@mail.umkc.edu>
2016-09-07 11:00:38 -0500

    removed commented coded and added balsamiqs

--- lab_2/Source/lab-2/routes/index.js
+++ lab_2/Source/lab-2/routes/index.js
@@ -1,13 +1,3 @@
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-//
-// module.exports = router;
-
 //
 // Module Dependencies
 //
@@ -28,61 +18,29 @@ module.exports = function(app) {
 
     /* GET home page. */
     app.get('/', function(req, res, next) {
-        //res.render('index', { title: 'Express' });
-        console.log('in \'/\'');
-
-//        res.render(homeTemplate, {title: 'Lab-1'});
+        // console.log('in \'/\'');
         res.render(loginTemplate);
     });
 
-    // Need to provide better error handling, see the following:
-    // http://stackoverflow.com/questions/15711127/express-passport-node-js-error-handling
     app.post('/login', function(req, res) {
-            // Successful auth, also note that passport sets the "user" JSON object found in db onto the req.
-            console.log('req.body = ' + req.body);
-            console.log('res = ' + req);
-            // if(req.user.TYPE_CD == 1) { //'educator'
-            //     return res.redirect('/educator_dashboard');
-            // } else if(req.user.TYPE_CD == 2) {  //'student'
-            //     return res.redirect('/student_dashboard');
-            // }
-
-            // update to some error page..
+        // console.log('in \'/login\'');
             return res.redirect('/login');
         }
     );
 
     /* GET register page. */
     app.get('/register', function(req, res, next) {
-        //res.render('index', { title: 'Express' });
-        console.log('in \'/register\'');
-
+        // console.log('in \'/register\'');
         res.render(registerTemplate);
     });
 
-    // Need to provide better error handling, see the following:
-    // http://stackoverflow.com/questions/15711127/express-passport-node-js-error-handling
     app.post('/register', function(req, res) {
-            // Successful auth, also note that passport sets the "user" JSON object found in db onto the req.
-            console.log('req.body = ' + req.body);
-            console.log('res = ' + req);
-            // if(req.user.TYPE_CD == 1) { //'educator'
-            //     return res.redirect('/educator_dashboard');
-            // } else if(req.user.TYPE_CD == 2) {  //'student'
-            //     return res.redirect('/student_dashboard');
-            // }
-
-            // update to some error page..
             return res.redirect('/');
         }
     );
 
     /* GET home page. */
     app.get('/home', function(req, res, next) {
-        //res.render('index', { title: 'Express' });
-        console.log('in \'/home\'');
-        console.log('homeTemplate=' + homeTemplate);
-
         // res.render(homeTemplate, {layout: 'home_layout'});
         res.render(homeTemplate);
     });
