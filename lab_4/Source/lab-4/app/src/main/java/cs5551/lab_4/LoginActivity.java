01f59a0fe145281791a1172220307c19e89ceac1
Wayne Aulner <wa6c6@mail.umkc.edu>
2016-09-21 13:32:41 -0500

    added speach to text functionality

--- lab_4/Source/lab-4/app/src/main/java/cs5551/lab_4/LoginActivity.java
+++ lab_4/Source/lab-4/app/src/main/java/cs5551/lab_4/LoginActivity.java
@@ -3,6 +3,7 @@ package cs5551.lab_4;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -49,7 +50,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "admin@whatever.com:admin", "wayne@whatever.com:wayne"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -187,16 +188,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            startActivity(new Intent(getApplicationContext(), SomeActivity.class));
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
+        //TODO: If this were a real app we would beef this validation up.
         return email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
+        //TODO: If this were a real app we would beef this validation up.
         return password.length() > 4;
     }
 
