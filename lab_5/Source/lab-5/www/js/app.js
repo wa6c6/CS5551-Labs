e1bf67f1aebcabb39db9cb87ca01d568b99647bd
Wayne Aulner <wa6c6@mail.umkc.edu>
2016-09-28 23:07:24 -0500

    lab-5 map working

--- lab_5/Source/lab-5/www/js/app.js
+++ lab_5/Source/lab-5/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
 .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -46,8 +46,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
     .state('home', {
         url: '/home',
-        templateUrl: 'templates/home.html'
-        //controller: '?'
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
     })
 
     .state('register', {
--- lab_5/Source/lab-5/www/js/controllers.js
+++ lab_5/Source/lab-5/www/js/controllers.js
@@ -1,22 +1,35 @@
 angular.module('starter.controllers', [])
 
-.controller('RegisterCtrl', function ($scope, $state) {
+.controller('HomeCtrl', function ($scope, $state, $cordovaGeolocation) {
+    var options = { timeout: 10000, enableHighAccuracy: true };
 
-    $scope.goRegister = function () {
-        //alert("Register!");
-        $state.go('/H')
-  };
+    $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
+
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        var mapOptions = {
+            center: latLng,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    }, function (error) {
+        console.log("Could not get location");
+    });
 })
 
 .controller('RegisterCtrl', function ($scope, $state, formData) {
     $scope.user = {};
 
     $scope.submitForm = function (user) {
-        if (user.firstName || user.lastName || user.comments) {
+        if (user.firstName && user.lastName) {
             formData.updateForm(user);
+//            alert("user.firstName:" + formData.getForm().firstName);
             $state.go('login');
         } else {
-            alert("Please fill out at least one field.");
+            alert("Please fill out at least first and last name.");
         }
     };
 
