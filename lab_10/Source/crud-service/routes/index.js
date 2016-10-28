ab0043f0c6726c0d4dfce8ec02778d44354edb0e
Wayne Aulner <wa6c6@mail.umkc.edu>
2016-10-27 22:51:26 -0500

    all but delete working

--- lab_10/Source/crud-service/routes/index.js
+++ lab_10/Source/crud-service/routes/index.js
@@ -11,125 +11,296 @@ var assert = require('assert');
 // Connection URL
 var url = 'mongodb://admin:password@ds145385.mlab.com:45385/lab_10';
 
-var findDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('user');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.dir(docs);
-    callback(docs);
-  });
-}
+var findAllDocuments = function(db, res, callback) {
+    // Get the user collection
+    var collection = db.collection('user');
+    // Find all users
+    collection.find({}).toArray(function(err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.dir(docs);
+        callback(docs);
+    });
+};
+
+var findDocument = function(db, query, res, callback) {
+    // Get the user collection
+    var collection = db.collection('user');
+    // query
+    console.log('query.email = ' + query.email);
+    // Find specific user
+    collection.find(query).toArray(function(err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.dir(docs);
+        callback(docs)
+    });
+};
+
+var insertDocument = function(db, item, res, callback) {
+    // Get the documents collection
+    var collection = db.collection('user');
+    // Insert some documents
+    collection.insertOne(item, function(err, result) {
+        assert.equal(err, null);
+        console.log("Inserted document into collection");
+        callback(result);
+    });
+};
+
+var updateDocument = function(db, key, set, res, callback) {
+    // Get the documents collection
+    var collection = db.collection('user');
+    // Update document where a is 2, set b equal to 1
+    collection.updateOne(key , set, function(err, result) {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            console.log("Updated the document");
+            callback(result);
+        });
+};
+
 
 //
 // Expose
 //
 module.exports = function(app) {
 
-  // Layouts
-  var baseLayout = path.join(app.settings.layoutsDir, 'layout');
-  // Templates
-  var loginTemplate  = path.join(app.settings.templatesDir, 'login');
-  var signupTemplate = path.join(app.settings.templatesDir, 'signup');
+    // Layouts
+    var baseLayout = path.join(app.settings.layoutsDir, 'layout');
+    // Templates
+    var loginTemplate = path.join(app.settings.templatesDir, 'login');
+    var signupTemplate = path.join(app.settings.templatesDir, 'signup');
+    var usersTemplate = path.join(app.settings.templatesDir, 'users');
+    var userDetailsTemplate = path.join(app.settings.templatesDir, 'user_details');
 
-  /* GET login page. */
-  app.get('/', function(req, res, next) {
-    console.log('in \'/\'');
+    /* GET login page. */
+    app.get('/', function(req, res, next) {
+        console.log('in \'/\'');
 
-    res.render(loginTemplate);
-  });
+        res.render(loginTemplate);
+    });
 
-  /* GET signup page. */
-  app.get('/signup', function(req, res, next) {
-    console.log('in \'/signup\'');
+    /* GET signup page. */
+    app.get('/signup', function(req, res, next) {
+        console.log('in \'/signup\'');
 
-    res.render(signupTemplate);
-  });
+        res.render(signupTemplate);
+    });
 
-  /* GET users page. */
-  app.get('/users', function(req, res, next) {
-    console.log('in \'/users\'');
+    /* POST signup page. */
+    app.post('/signup', function(req, res, next) {
+        console.log('in post \'/signup\'');
 
-    // lists all users in db
-    res.render(signupTemplate);
-  });
+        console.log('req.body.firstName =', req.body.firstName);
 
-  /* GET user_detail page. */
-  app.get('/user_detail', function(req, res, next) {
-    console.log('in \'/user_detail\'');
+        // Use connect method to connect to the Server
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to server");
 
-    // displays all user information
-    // allow delete/update
-    res.render(signupTemplate);
-  });
+            var item = { email: req.body.email,
+                         firstName: req.body.firstName,
+                         lastName : req.body.lastName,
+                         address: req.body.address,
+                         city: req.body.city,
+                         state: req.body.state,
+                         zip: req.body.zip,
+                         phone: req.body.phone };
 
-  /* GET  page. */
-  app.get('/read', function(req, res, next) {
-    console.log('in \'/read\'');
+            // Save
+            insertDocument(db, item, res, function(){
+                db.close();
 
-    console.log('req.body.txt =', req.body.txt);
+                // redirect to users
+                res.redirect('/users');
+            });
 
-    // Use connect method to connect to the Server
-    MongoClient.connect(url, function(err, db) {
-      assert.equal(null, err);
-      console.log("Connected correctly to server");
+        });
+    });
 
-      findDocuments(db, function(){
+    /* GET users page. */
+    app.get('/users', function(req, res, next) {
+        console.log('in \'/users\'');
 
-        db.close();
+        // fetch all users
+        // Use connect method to connect to the Server
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to server");
+
+            findAllDocuments(db, res, function(docs) {
+                db.close();
+
+                // loop
+                var rows;
+                for(var i = 0; i < docs.length; i++) {
+
+                    rows = rows +
+                           "<tr>" +
+                           "<td><a href=\"/user_details?user=" + docs[i].email + "\">" + docs[i].firstName + "</a></td>" +
+                           "<td>" + docs[i].lastName + "</td>" +
+                           "<td>" + docs[i].email + "</td>" +
+                           "</tr>";
+                }
+
+                res.locals.tableRows = rows;
+
+                res.render(usersTemplate);
+            });
+        });
+    });
+
+    /* GET user_detail page. */
+    app.get('/user_details', function(req, res, next) {
+        console.log('in \'/user_details\'');
+
+        // console.log(req);
+        console.log("req.query.user = ", req.query.user);
+
+        // Use connect method to connect to the Server
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to server");
+
+            var query = {email: req.query.user};
+            findDocument(db, query, res, function(docs) {
+                db.close();
+
+                console.log('address = ' + docs[0].address);
+
+                res.locals.email = docs[0].email;
+                res.locals.firstName = docs[0].firstName;
+                res.locals.lastName = docs[0].lastName;
+                res.locals.address = docs[0].address;
+                res.locals.city = docs[0].city;
+                res.locals.state = docs[0].state;
+                res.locals.zip = docs[0].zip;
+                res.locals.phone = docs[0].phone;
+
+                res.render(userDetailsTemplate);
+            });
+        });
+    });
+
+    /* GET delete_user page. */
+    app.get('/delete_user', function(req, res, next) {
+        console.log('in \'/delete_user\'');
+
+        // delete user
+        res.redirect('/users');
+
+    });
+
+    /* GET update_user page. */
+    app.post('/update_user', function(req, res, next) {
+        console.log('in \'/update_user\'');
+
+        console.log("req.body.email = ", req.body.email);
+
+        // Use connect method to connect to the Server
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to server");
+
+            var key = { email: req.body.email };
+            var set = {$set: {firstName: req.body.firstName,
+                              lastName: req.body.lastName,
+                              address: req.body.address,
+                              city: req.body.city,
+                              state: req.body.state,
+                              zip: req.body.zip,
+                              phone: req.body.phone} };
+
+            // Update
+            updateDocument(db, key, set, res, function(result){
+                db.close();
+
+                // redirect to users
+                res.redirect('/users');
+            });
+
+        });
+    });
+
+    /* Post read page. */
+    app.post('/read', function(req, res, next) {
+        console.log('in \'/read\'');
+
+        // console.log(req);
+        console.log('req.body.email=' + req.body.email);
+
+        // Use connect method to connect to the Server
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to server");
+
+            var query = {email: req.body.email};
+            findDocument(db, query, res, function(docs) {
+                if(docs.length > 0) {
+                    console.log('registered');
+                    findAllDocuments(db, res, function(docs) {
+                        db.close();
+
+                        console.log('redirect.. ');
+                        // return res.redirect('/users');
+                        //   return '/users';
+                        res.redirect('/users');
+                    });
+                }
+                else {
+                    db.close();
+                    console.log('not registered');
+                    console.log('redirect..');
+                    res.redirect('/signup');
+                }
+            });
+
+        });
+    });
+
+    /* GET  page. */
+    app.get('/read', function(req, res, next) {
+        console.log('in \'/read\'');
+
+        // Use connect method to connect to the Server
+        // MongoClient.connect(url, function(err, db) {
+        //   assert.equal(null, err);
+        //   console.log("Connected correctly to server");
+        //
+        //   db.close();
+        //
+        // });
 
         // If not found
         console.log('redirect..');
         return res.redirect('/signup');
-      })
+
     });
 
-  });
-
-  /* GET  page. */
-  app.get('/read', function(req, res, next) {
-    console.log('in \'/read\'');
-
-    // Use connect method to connect to the Server
-    // MongoClient.connect(url, function(err, db) {
-    //   assert.equal(null, err);
-    //   console.log("Connected correctly to server");
     //
-    //   db.close();
+    // error handlers
     //
-    // });
 
-    // If not found
-    console.log('redirect..');
-    return res.redirect('/signup');
+    // development error handler
+    // will print stacktrace
+    if(app.get('env') === 'development') {
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
 
-  });
-
-  //
-  // error handlers
-  //
-
-  // development error handler
-  // will print stacktrace
-  if (app.get('env') === 'development') {
+    // production error handler
+    // no stacktraces leaked to user
     app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
     });
-  }
-
-  // production error handler
-  // no stacktraces leaked to user
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {}
-    });
-  });
 };
